cdb6a2b077af72deee408d276c7a38173cc673c7
bubet <bubetamileva07@gmail.com>
2026-01-23 12:48:16 +0200

    .

---
+++ FinalProject/Migration/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FinalProject.Models;
+
+namespace FinalProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Route> Routes { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+    }
+}

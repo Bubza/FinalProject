75f03a2fcdc247cd5635e118a4a62e46912ff4d7
bubet <bubetamileva07@gmail.com>
2026-03-27 21:15:10 +0200

    Payment Services

---
+++ Tourism.Services/IPaymentService.cs
@@ -0,0 +1,11 @@
+﻿using Tourism.Data.Models.Entities;
+
+namespace Tourism.Services
+{
+    public interface IPaymentService
+    {
+        Task<IEnumerable<Payment>> GetAllAsync();
+        Task<Payment?> GetByBookingIdAsync(int bookingId);
+        Task CreateAsync(Payment payment);
+    }
+}

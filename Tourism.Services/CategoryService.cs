cca88a1ae73c3e66e2e9486bc329ffd10d4fdc42
bubet <bubetamileva07@gmail.com>
2026-03-27 21:07:10 +0200

    CategoryService

--- Tourism.Services/CategoryService.cs
+++ Tourism.Services/CategoryService.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Tourism.Data;
+using Tourism.Data.Models.Entities;
 
 namespace Tourism.Services
 {
-    internal class CategoryService
+    public class CategoryService : ICategoryService
     {
+        private readonly ApplicationDbContext _context;
+        public CategoryService(ApplicationDbContext context) => _context = context;
+
+        public async Task<IEnumerable<Category>> GetAllAsync() =>
+            await _context.Categories.Include(c => c.Tours).ToListAsync();
+
+        public async Task<Category?> GetByIdAsync(int id) =>
+            await _context.Categories.Include(c => c.Tours).FirstOrDefaultAsync(c => c.Id == id);
     }
 }
--- Tourism.Services/ICategoryService.cs
+++ Tourism.Services/ICategoryService.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Tourism.Data.Models.Entities;
 
 namespace Tourism.Services
 {
-    internal class ICategoryService
+    public interface ICategoryService
     {
+        Task<IEnumerable<Category>> GetAllAsync();
+        Task<Category?> GetByIdAsync(int id);
     }
 }

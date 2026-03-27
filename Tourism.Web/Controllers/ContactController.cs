f1bf0131bd5d33fe05e5a15ae86cd2977a86b246
bubet <bubetamileva07@gmail.com>
2026-03-27 21:17:44 +0200

    Contact Controller added

---
+++ Tourism.Web/Controllers/ContactController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tourism.Data.Models.Entities;
+using Tourism.Services;
+
+namespace Tourism.Web.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IContactMessageService _contactService;
+
+        public ContactController(IContactMessageService contactService)
+        {
+            _contactService = contactService;
+        }
+
+        // GET: /Contact
+        public IActionResult Index() => View();
+
+        // POST: /Contact
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index(ContactMessage model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            await _contactService.CreateAsync(model);
+            TempData["Success"] = "Your message has been sent! We'll get back to you within 24 hours.";
+            return RedirectToAction(nameof(Success));
+        }
+
+        public IActionResult Success() => View();
+    }
+}

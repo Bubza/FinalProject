aeebf2bc86a120034d9d2990c5d67fe05b01e011
bubet <bubetamileva07@gmail.com>
2026-03-27 20:29:01 +0200

    ContactMessage

--- Tourism.Data/Models/Entities/ContactMessage.cs
+++ Tourism.Data/Models/Entities/ContactMessage.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Tourism.Data.Models.Entities
+﻿namespace Tourism.Data.Models.Entities
 {
-    internal class ContactMessage
+    public class ContactMessage
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Subject { get; set; } = string.Empty;
+
+        public string Message { get; set; } = string.Empty;
+
+        public DateTime SentAt { get; set; } = DateTime.UtcNow;
+
+        public bool IsRead { get; set; } = false;
     }
 }

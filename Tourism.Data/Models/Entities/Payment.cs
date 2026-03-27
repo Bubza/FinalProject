909ae8f348bc753eec13ace1d19ba878b535f147
bubet <bubetamileva07@gmail.com>
2026-03-27 20:28:27 +0200

    Payment Entity

--- Tourism.Data/Models/Entities/Payment.cs
+++ Tourism.Data/Models/Entities/Payment.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Tourism.Data.Models.Entities
+﻿namespace Tourism.Data.Models.Entities
 {
-    internal class Payment
+    public class Payment
     {
+        public int Id { get; set; }
+
+        public int BookingId { get; set; }
+
+        public decimal Amount { get; set; }
+
+        // "Card", "Borica", "Cash"
+        public string Method { get; set; } = "Card";
+
+        // "Paid", "Pending", "Refunded"
+        public string Status { get; set; } = "Paid";
+
+        public string TransactionId { get; set; } = string.Empty;
+
+        public DateTime PaidAt { get; set; } = DateTime.UtcNow;
+
+        // Navigation
+        public Booking Booking { get; set; } = null!;
     }
 }

9b20ba9283380cd9bd02abaaf09f325471d1571b
bubet <bubetamileva07@gmail.com>
2026-03-28 17:09:10 +0200

    OperatorDashboardViewModel

--- Tourism.Web/Models/ViewModels/OperatorDashboardViewModel.cs
+++ Tourism.Web/Models/ViewModels/OperatorDashboardViewModel.cs
@@ -1,6 +1,21 @@
-﻿namespace Tourism.Web.Models.ViewModels
+﻿using Tourism.Data.Models.Entities;
+
+namespace Tourism.Web.Models.ViewModels
 {
     public class OperatorDashboardViewModel
     {
+        public TourOperator Operator { get; set; } = null!;
+        public int TotalTours { get; set; }
+        public int TotalBookings { get; set; }
+        public int PendingBookings { get; set; }
+        public decimal Revenue { get; set; }
+        public List<OperatorPopularTourItem> PopularTours { get; set; } = new();
+    }
+
+    public class OperatorPopularTourItem
+    {
+        public string Title { get; set; } = string.Empty;
+        public int BookingCount { get; set; }
+        public decimal PricePerPerson { get; set; }
     }
 }

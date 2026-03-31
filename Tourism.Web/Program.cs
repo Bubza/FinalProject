03a372a7e0db54bfaf2e72219a4aa1b06d1f9985
bubet <bubetamileva07@gmail.com>
2026-04-01 01:35:19 +0300

    .

--- Tourism.Web/Program.cs
+++ Tourism.Web/Program.cs
@@ -6,21 +6,26 @@ using Tourism.Services;
 using System.Globalization;
 using Tourism.Data.Models.Entities;
 using Tourism.Data.Configurations;
+
 var cultureInfo = new CultureInfo("de-DE");
 CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
 CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
+
 var builder = WebApplication.CreateBuilder(args);
+
 // Database
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
     ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 // Identity
 builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
     options.SignIn.RequireConfirmedAccount = false)
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
 // Services
 builder.Services.AddScoped<ITourService, TourService>();
 builder.Services.AddScoped<IBookingService, BookingService>();
@@ -31,6 +36,7 @@ builder.Services.AddScoped<IFavoriteTourService, FavoriteTourService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<IContactMessageService, ContactMessageService>();
 builder.Services.AddScoped<IPaymentService, PaymentService>();
+
 // Add MVC Controllers + Razor Pages
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
@@ -38,8 +44,9 @@ builder.Services.Configure<RequestLocalizationOptions>(options =>
 {
     options.DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture("en-US");
 });
+
 var app = builder.Build();
-// Seed admin role and user
+
 // Auto-migrate on startup
 using (var scope = app.Services.CreateScope())
 {
@@ -48,6 +55,7 @@ using (var scope = app.Services.CreateScope())
     await AdminSeeder.SeedAsync(scope.ServiceProvider);
     await DemoDataSeeder.SeedAsync(scope.ServiceProvider);
 }
+
 // Middleware pipeline
 if (app.Environment.IsDevelopment())
 {
@@ -58,16 +66,19 @@ else
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
+
 app.UseRequestLocalization("en-US");
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
+
 // MVC default route
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
 // Razor Pages (for Identity UI)
 app.MapRazorPages();
 app.Run();
